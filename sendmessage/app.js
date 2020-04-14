028b9318d5aa0786f2728711ef4d00399a87ce34
Jonathan Rohland <jonathan.rohland@tngtech.com>
2020-04-14 08:03:57 +0200

    Rename connections_table

--- sendmessage/app.js
+++ sendmessage/app.js
@@ -5,37 +5,38 @@ const AWS = require('aws-sdk');
 
 const ddb = new AWS.DynamoDB.DocumentClient({ apiVersion: '2012-08-10', region: process.env.AWS_REGION });
 
-const { TABLE_NAME } = process.env;
+const { CONNECTIONS_TABLE_NAME } = process.env;
 
 exports.handler = async event => {
-  let connectionData;
-  
+  let openConnections;
+
   try {
-    connectionData = await ddb.scan({ TableName: TABLE_NAME, ProjectionExpression: 'connectionId' }).promise();
+    openConnections = await ddb.scan({ TableName: CONNECTIONS_TABLE_NAME, ProjectionExpression: 'connectionId' }).promise();
   } catch (e) {
     return { statusCode: 500, body: e.stack };
   }
-  
+
   const apigwManagementApi = new AWS.ApiGatewayManagementApi({
     apiVersion: '2018-11-29',
     endpoint: event.requestContext.domainName + '/' + event.requestContext.stage
   });
-  
+
   const postData = JSON.parse(event.body).data;
-  
-  const postCalls = connectionData.Items.map(async ({ connectionId }) => {
+
+
+  const postCalls = openConnections.Items.map(async ({ connectionId }) => {
     try {
       await apigwManagementApi.postToConnection({ ConnectionId: connectionId, Data: postData }).promise();
     } catch (e) {
       if (e.statusCode === 410) {
         console.log(`Found stale connection, deleting ${connectionId}`);
-        await ddb.delete({ TableName: TABLE_NAME, Key: { connectionId } }).promise();
+        await ddb.delete({ TableName: CONNECTIONS_TABLE_NAME, Key: { connectionId } }).promise();
       } else {
         throw e;
       }
     }
   });
-  
+
   try {
     await Promise.all(postCalls);
   } catch (e) {
